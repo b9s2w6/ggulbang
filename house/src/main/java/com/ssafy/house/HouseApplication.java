2404b62cc071be645c6a22fba371c6ec1c24688b
최진학 <cheshire99.dev@gmail.com>
2023-11-03 13:16:53 +0900

    basic code

---
+++ house/src/main/java/com/ssafy/house/HouseApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("working");
+		SpringApplication.run(HouseApplication.class, args);
+		
+	}
+
+}
