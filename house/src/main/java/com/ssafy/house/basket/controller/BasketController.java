ac22c42a07de20362dafbfebe7919ddfbd9d801a
최진학 <cheshire99.dev@gmail.com>
2023-11-03 13:37:18 +0900

    basic code

---
+++ house/src/main/java/com/ssafy/house/basket/controller/BasketController.java
@@ -0,0 +1,12 @@
+package com.ssafy.house.basket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 231101 | 성욱 | basic -> basicName 으로 변경 
+
+@Controller
+@RequestMapping("/basket")
+public class BasketController {
+	 
+}
