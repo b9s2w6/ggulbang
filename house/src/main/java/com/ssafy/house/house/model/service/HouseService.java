049b64dd7d9d035f273f26d66c1c10116f458c14
BaekSeongUk <b9s2w6@naver.com>
2023-11-10 14:52:22 +0900

    231110 아파트 crd 구현

--- house/src/main/java/com/ssafy/house/house/model/service/HouseService.java
+++ house/src/main/java/com/ssafy/house/house/model/service/HouseService.java
@@ -2,12 +2,15 @@ package com.ssafy.house.house.model.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ssafy.house.house.repository.House;
+
 
 public interface HouseService {
 	List<House> selectHouseAll();
-	House selectHouseDetail(long aptNo);
+	House selectHouseDetail(long aptCode);
 	int registHouse(House House);
 	int updateHouse(House House);
-	int deleteHouse(long aptNo);
+	int deleteHouse(long aptCode);
 }
