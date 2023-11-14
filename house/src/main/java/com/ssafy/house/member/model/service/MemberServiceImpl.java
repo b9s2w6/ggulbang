59527818b92a3c112249cad56c4565818f653681
BaekSeongUk <b9s2w6@naver.com>
2023-11-14 17:48:37 +0900

    231114 백성욱 jwt 토큰구현

--- house/src/main/java/com/ssafy/house/member/model/service/MemberServiceImpl.java
+++ house/src/main/java/com/ssafy/house/member/model/service/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package com.ssafy.house.member.model.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.house.house.model.mapper.HouseMapper;
 import com.ssafy.house.member.model.mapper.MemberMapper;
 import com.ssafy.house.member.repository.Member;
+import com.ssafy.house.util.Encryption;
+import com.ssafy.house.util.JWTUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,8 +21,20 @@ public class MemberServiceImpl implements MemberService {
 	final MemberMapper memberMapper;
 	
 	@Override
-	public Member login(Member member) {
-		return memberMapper.login(member);
+	public Map<String, String> login(Member member) {
+		Map<String, String> token = new HashMap<>();
+		Member memberInfo = memberMapper.login(member);
+		if(Encryption.checkPW(member.getUserPass(), memberInfo.getUserPass())){
+			token.put("token", JWTUtil.generateToken(memberInfo));
+			return token;
+		}
+		return null;
+	}
+	
+	@Override
+	public void registMember(Member member) {
+		member.setUserPass(Encryption.hashPW(member.getUserPass()));
+		memberMapper.registMember(member);
 	}
 	
 }
