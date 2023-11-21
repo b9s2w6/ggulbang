2d4f2865cde63ad2a8a61ec15006e79f77615f40
백성욱 <b9s2w6@naver.com>
2023-11-22 03:12:09 +0900

    231121 백성우 상품추가 구현

--- house/src/main/java/com/ssafy/house/house/model/service/HouseService.java
+++ house/src/main/java/com/ssafy/house/house/model/service/HouseService.java
@@ -3,6 +3,8 @@ package com.ssafy.house.house.model.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
+
 import com.ssafy.house.house.repository.House;
 import com.ssafy.house.house.repository.HouseList;
 
@@ -11,7 +13,10 @@ public interface HouseService {
 	HouseList selectHouseAll(Map<String, String> map);
 	List<House> selectMyHouseAll(String userId);
 	House selectHouseDetail(long aptCode);
-	int registHouse(House House);
+	int registHouse(Map<String, String> map);
 	int updateHouse(Map<String, Object> params);
 	int deleteHouse(long aptCode);
+	List<String> getSidoList();
+	List<String> getGugunList(String sidoName);
+	List<String> getDongList(Map<String, String> map);
 }
